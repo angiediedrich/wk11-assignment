5b757ed110b4354f05fb78d201f6419b22143dec
angiediedrich <adiedrich@mcad.edu>
2017-11-12 10:53:15 -0600

    first sublist li text color change

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ $(document).ready(function () {
 
 //Assignment 11
 //Add the attached icon image before the text in each LI element in the main navigation
-$('#menu ul li a').prepend('<img id="theIcon" src="icon.png" />')
+$('#menu ul li a').prepend('<img id="theIcon" src="icon.png" />');
 //Clicking on the H1 element in the header changes the background color of the header DIV
 $("h1").click(function () {
-      $("#header").css("background","purple");
-    });
+	$("#header").css("background","purple");
+});
 //Change the text color of the first LI element in one of the sub-lists in the sidebar
+$('#sidebar_content_1 li a:first').css("color","blue");
 //Move the position of the even “numbered” LI elements in one of the sub-lists in the sidebar to the right by 5 pixels
 //Clicking on one of the post titles in the main content area replaces the content of the content DIV (with the ID of “content”) with content you specify
 //Clicking on one of the H2 elements in the sidebar toggles visibility of the list below it
